0d70b4084c098b39aeead0cd6fcbf2ec71517889
AlexTaglia <alex.tagliabue@gmail.com>
2021-09-13 15:32:31 +0200

    Creazione seeds

---
+++ database/seeds/PostsTableSeeders.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Post;
+use Faker\Generator as Faker;
+
+class PostsTableSeeders extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i = 0; $i < 10; $i++){
+
+            $bookObject = new Post();
+            $bookObject->title = $faker->sentence(5);  
+            $bookObject->content = $faker->paragraph(5);
+            $bookObject->img = $faker->imageUrl(640, 480, 'post', true);
+            $bookObject->save();
+        }
+    }
+}
